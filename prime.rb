9c07013cd59a0e5a0d8311faa05bd5381a388b97
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-03 16:33:14 -0400

    prime numbers solution

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  elsif
+    for i in 2..(n - 1)
+      if n % i == 0
+        return false
+      end
+    end
+  end
+  return true
+end
